501a976ebac12fd0f6cfc1bec29fe312301c8a41
alouzizi <alouzizi@student.1337.ma>
2023-01-29 13:28:47 +0100

    Fixed error when replacing occurrences word with a new word of larger length.

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,49 +6,44 @@
 /*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:52:37 by alouzizi          #+#    #+#             */
-/*   Updated: 2023/01/28 21:13:35 by alouzizi         ###   ########.fr       */
+/*   Updated: 2023/01/29 13:27:52 by alouzizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
 #include <fstream>
 
 std::string replace(std::string line, char **av)
 {
-	int	i = 0;
-	int	j =0;
-	int p = 0;
+	std::string newline;
+	int i;
 
-	while (i < (int) line.length())
+	i = 0;
+	while ((i = line.find(av[2], i)) != (int)std::string::npos)
 	{
-		p = line.find(av[2],i);
-		if(p != (int) std::string::npos)
-		{
-			while(av[3][j])
-				line[p++] = av[3][j++];
-			p -= ((std::string )av[3]).length();
-			i = p + ((std::string )av[3]).length();
-			j = 0;
-		}
-		else
-			break;
+		newline.append(line, 0, i);
+		newline.append(av[3]);
+		i += ((std::string)av[2]).length();
+		line.erase(0, i);
+		i = 0;
 	}
-	return (line);
+	newline.append(line);
+	return (newline);
 }
 
 int main(int ac, char **av)
 {
-	std::ifstream	inputfile;
-	std::ofstream	outputfile;
-	std::string		line;
+	std::ifstream inputfile;
+	std::ofstream outputfile;
+	std::string line;
 
 	if (ac != 4)
 	{
 		std::cerr << "invalide argiments\n";
 		return (1);
 	}
-	inputfile.open (av[1]);
+	inputfile.open(av[1]);
 	if (inputfile.fail())
 	{
 		std::cerr << "Errore opening file\n";
@@ -60,7 +55,7 @@ int main(int ac, char **av)
 		std::cerr << "Errore opening file\n";
 		return (1);
 	}
-	while(std::getline(inputfile, line))
+	while (std::getline(inputfile, line))
 	{
 		line = replace(line, av);
 		outputfile << line << "\n";
