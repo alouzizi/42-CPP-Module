952c960464cb152023b93c76befa7ffcc9e0f710
alouzizi <alouzizi@student.1337.ma>
2023-07-20 15:49:14 +0100

    ex02 done

--- cpp_module_06/ex02/Base.cpp
+++ cpp_module_06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 10:55:46 by alouzizi          #+#    #+#             */
-/*   Updated: 2023/07/20 14:07:25 by alouzizi         ###   ########.fr       */
+/*   Updated: 2023/07/20 15:29:50 by alouzizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,52 @@ Base::~Base() {}
 
 Base *generate(void)
 {
-    int rand = std::rand() % 3 + 1;
-    if (rand == 1)
-        return new A();
-    else if (rand == 2)
-        return new B();
-    else
-        return new C();
+	int rand = std::rand() % 3 + 1;
+	if (rand == 1)
+		return new A();
+	else if (rand == 2)
+		return new B();
+	else
+		return new C();
 }
 
 void identify(Base *p)
 {
-    if (dynamic_cast<A *>(p))
-        std::cout << "A" << std::endl;
-    else if (dynamic_cast<B *>(p))
-        std::cout << "B" << std::endl;
-    else if (dynamic_cast<C *>(p))
-        std::cout << "c" << std::endl;
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "c" << std::endl;
 }
 
 void identify(Base &p)
 {
-    identify(&p);
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+		(void)a;
+	}
+	catch (const std::bad_cast &)
+	{
+	}
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+		(void)b;
+	}
+	catch (const std::bad_cast &)
+	{
+	}
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+		(void)c;
+	}
+	catch (const std::bad_cast &)
+	{
+	}
 }
