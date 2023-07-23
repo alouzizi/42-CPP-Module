73d86bb919cb858a1adaa736944b6ac71400af8e
alouzizi <alouzizi@student.1337.ma>
2023-07-23 16:07:13 +0100

    Implemented generic swap, min, and max functions for flexible value manipulation.

---
+++ cpp_module_07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 11:37:22 by alouzizi          #+#    #+#             */
+/*   Updated: 2023/07/23 16:03:56 by alouzizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	return 0;
+}
---
+++ cpp_module_07/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 11:37:28 by alouzizi          #+#    #+#             */
+/*   Updated: 2023/07/23 16:03:50 by alouzizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+    return (a > b) ? a : b;
+}
+
+#endif
