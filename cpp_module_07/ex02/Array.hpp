5bce81fde69325501b463162e14952d31e7e9df0
alouzizi <alouzizi@student.1337.ma>
2023-07-26 15:16:59 +0100

    Implemented a template Array class

---
+++ cpp_module_07/ex02/Array.hpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 13:12:38 by alouzizi          #+#    #+#             */
+/*   Updated: 2023/07/26 14:38:43 by alouzizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	T *_array;
+	unsigned int _nb;
+
+public:
+	Array()
+	{
+		_array = new T[0];
+		_nb = 0;
+	}
+	~Array()
+	{
+		delete[] _array;
+	}
+	Array(unsigned int n)
+	{
+		_array = new T[n];
+		_nb = n;
+	}
+	Array(Array const &array)
+	{
+		_array = new T[array._nb];
+		_nb = array._nb;
+		for (unsigned int i = 0; i < _nb; i++)
+			_array[i] = array._array[i];
+	}
+	Array &operator=(Array const &array)
+	{
+		if (this != &array)
+		{
+			delete[] _array;
+			_array = new T[array._nb];
+			_nb = array._nb;
+			for (unsigned int i = 0; i < _nb; i++)
+				_array[i] = array._array[i];
+		}
+		return (*this);
+	}
+	T &operator[](unsigned int i)
+	{
+		if (i >= _nb)
+			throw std::exception();
+		return (_array[i]);
+	}
+	unsigned int size() const
+	{
+		return (_nb);
+	}
+};
+
+#endif
---
+++ cpp_module_07/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 13:13:44 by alouzizi          #+#    #+#             */
+/*   Updated: 2023/07/26 15:15:18 by alouzizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main()
+{
+	try
+	{
+		std::string s[] = {"Hello", "World", "!"};
+		Array<int> a(5);
+		Array<std::string> b(3);
+		for (int i = 0; i < 5; i++)
+		{
+			a[i] = i;
+			std::cout << a[i] << std::endl;
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			b[i] = s[i];
+			std::cout << b[i] << std::endl;
+		}
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
