dfe7d36821a46e88294f81a9451fb4bd192563b6
alouzizi <alouzizi@student.1337.ma>
2023-08-03 15:57:21 +0100

    implementing iterators for MutantStack

---
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alouzizi <alouzizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 10:39:53 by alouzizi          #+#    #+#             */
+/*   Updated: 2023/08/03 15:56:38 by alouzizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+#include <iostream>
+#include <vector>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin()
+	{
+		return std::stack<T>::c.begin();
+	}
+
+	iterator end()
+	{
+		return std::stack<T>::c.end();
+	}
+
+	const_iterator begin() const
+	{
+		return std::stack<T>::c.begin();
+	}
+
+	const_iterator end() const
+	{
+		return std::stack<T>::c.end();
+	}
+};
+
+#endif
